a48b8da813bcfbd04145fb75c55d5b2413b39dfc
cjo5929 <rkdalstj4505@naver.com>
2024-02-01 23:44:02 +0900

    update: 2024-02-01 boj_9663_N-Queen minseo

--- src/week1/G4_9663_NQueen/G4_9663_NQueen_minseo.java
+++ src/week1/G4_9663_NQueen/G4_9663_NQueen_minseo.java
@@ -6,74 +6,80 @@ import java.io.InputStreamReader;
 
 /**
  * @author 강민서
- * @date 2024.01.31
+ * @date 2024.02.01
  * @link https://www.acmicpc.net/problem/9663
- * @keyword_solution  N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제 => 퀸은 같은 행, 열, 대각선 공격 가능
- * @input (1 ≤ N < 15) -> 완탐 가능
- * @output  -> 경우의 수 출력
- * @time_complex  
- * @perf 
+ * @keyword_solution N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제 => 퀸은 같은 행, 열, 대각선 공격 가능
+ * @input (1 ≤ N < 15) -> 재귀를 통한 완탐 가능
+ * @output -> 경우의 수 출력
+ * @time_complex O(n!)
+ * @perf 18464	7984
  */
 
 public class G4_9663_NQueen_minseo {
-	
-	static int n, cnt;
-	static int[] arr;
+
+	static int n, cnt, x, y;
 	static boolean check;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		n = Integer.parseInt(br.readLine());
 		cnt = 0;
-		arr = new int[n];
+		int[][] arr = new int[n][n];
 		check = true;
-		/**
-		 * arr[index] = v => index가 열, v가 행으로 생각
-		 *  arr[0] = 2
-		 *  arr[1] = 0
-		 *  arr[2] = 3
-		 *  arr[3] = 1
-		 */
-		
-		cntQueen(0);
+
+		cntQueen(0, arr);
 		sb.append(cnt);
 		System.out.println(sb);
 
 	}
-	static void cntQueen(int depth) { // depth가 n과 같으면 끝
-		//base
-		if(depth == n) {
+
+	static void cntQueen(int depth, int[][] arr) { // depth가 n과 같으면 끝
+		// base
+		if (depth == n) {
 			cnt++;
 			return;
 		}
+
 		
-		for(int i = 0; i < n; i++) {
-			arr[depth] = i; // 일단 퀸 놓기
-			
-			// 퀸 조건
-			for(int j = 0; j < depth; j++) { // 같은 행 => 현재 depth열의 행과 i열의 행이 같을 경우 퀸 배치 불가능
-				if(arr[depth] == arr[j]) {
-					check = false; 
-					break; 
-				}
-				else if(Math.abs(depth - j) == Math.abs(arr[depth] - arr[j])) { // 대각선 => 열의 차와 행의 차가 같을 경우
-					check = false; 
-					break;
-				}
-				
-				check = true;
-				
-			}
-			
-			if(check) {
-				cntQueen(depth + 1);
-			}
-			
+        for (int i = 0; i < n; i++) {
+            if (check(arr, depth, i)) { // 유효성 검사 통과하면 퀸 놓기
+                arr[depth][i] = 1;
+                cntQueen(depth + 1, arr);
+                arr[depth][i] = 0; // 모든 경우의 수 탐색
+            }
+        }
+	}
+	
+	// 퀸 조건 검사 => 퀸을 놓을 수 있다면 depth를 높이기 depth 아래 놓는 퀸 조건은 제외
+	static boolean check(int[][] arr, int depth, int col) {
+		
+		// 열 검사
+		for (int i = 0; i < depth; i++) {
+			if(arr[i][col] == 1) return false;
 			
 		}
-	}
 		
+		// 왼쪽 위 대각선 검사
+		x = depth - 1;
+		y = col - 1;
+		while(x >= 0 && y >= 0) {
+			if(arr[x][y] == 1) return false;
+			x--;
+			y--;
+		}
+		
+		// 오른쪽 위 대각선 검사
+		x = depth - 1;
+		y = col + 1;
+		while(x >= 0 && y < n) {
+			if(arr[x][y] == 1) return false;
+			x--;
+			y++;
+		}
+		
+
+		return true;
+	}
 
 }
